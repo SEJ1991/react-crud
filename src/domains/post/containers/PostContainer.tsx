06428988256a879aeef708ecd72faf20e49038c1
SEJ <jink6111@gmail.com>
2025-03-21 20:25:54 +0900

    feat: [RC_busin-logic-post-13] write busin logic for post detail

--- src/domains/post/containers/PostContainer.tsx
+++ src/domains/post/containers/PostContainer.tsx
@@ -1,8 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
 import { PageFrame } from '../../../shared';
+import { getPost } from '../services/postService';
+import { useNavigate } from 'react-router';
 
 interface Props {
   id?: string;
 }
-export function PostContainer({ id }: Props) {
+export function PostContainer({ id = '' }: Props) {
+  const navigate = useNavigate();
+  const { data: post, isLoading } = useQuery({
+    queryKey: ['post', id],
+    queryFn: getPost(id),
+  });
+
+  const handleClickBackBtn = () => {
+    navigate('/posts');
+  };
+
+  if (isLoading) return <div>loading</div>;
+  if (!post) return <div>undefined post, this id is {id}.</div>;
   return <PageFrame>{id}</PageFrame>;
 }
