5837bccbf307ee367f8b483a58c20491c9489427
SEJ <jink6111@gmail.com>
2025-03-24 18:37:33 +0900

    refactor: [RC_declare-base-button-style-25] declare base button style and refactor button style

--- src/domains/post/components/Post.tsx
+++ src/domains/post/components/Post.tsx
@@ -9,7 +9,7 @@ export default function Post({ post: { title, body }, onClickBackBtn }: Props) {
   return (
     <AtomicCard>
       <button
-        className='p-2 rounded-md border cursor-pointer transition-colors duration-500 ease-in-out hover:text-red-400'
+        className='transition-colors duration-500 ease-in-out hover:text-red-400'
         onClick={onClickBackBtn}
       >
         Back to list
--- src/domains/user/components/User.tsx
+++ src/domains/user/components/User.tsx
@@ -20,7 +20,7 @@ export default function User({
   return (
     <AtomicCard>
       <button
-        className='p-2 rounded-md border cursor-pointer transition-colors duration-500 ease-in-out hover:text-red-400'
+        className='transition-colors duration-500 ease-in-out hover:text-red-400'
         onClick={onClickBackBtn}
       >
         Back to list
