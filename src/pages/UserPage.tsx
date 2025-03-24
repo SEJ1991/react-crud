fafa411ee3ce99187e6c3457ef25bf0a5b9f0141
SEJ <jink6111@gmail.com>
2025-03-24 18:07:10 +0900

    feat: [RC_build-user-23] build user page

---
+++ src/pages/UserPage.tsx
@@ -0,0 +1,10 @@
+import { UserContainer } from '../domains/user';
+import { PageFrame } from '../shared';
+
+export function UserPage() {
+  return (
+    <PageFrame>
+      <UserContainer />
+    </PageFrame>
+  );
+}
