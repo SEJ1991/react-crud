b451c29725e27405d28ec1fd8d130f0c48854486
SEJ <jink6111@gmail.com>
2025-03-24 18:26:53 +0900

    feat: [RC_busin-logic-user-24] add handleClickBackBtn fn

--- src/domains/user/containers/UserContainer.tsx
+++ src/domains/user/containers/UserContainer.tsx
@@ -1,14 +1,20 @@
 import { useQuery } from '@tanstack/react-query';
 import User from '../components/User';
 import { getUser } from '../services/userService';
+import { useNavigate } from 'react-router';
 
 interface Props {
   id?: string;
 }
 export function UserContainer({ id = '' }: Props) {
+  const navigate = useNavigate();
   const { data: user, isLoading } = useQuery({ queryKey: ['users', id], queryFn: getUser(id) });
+
+  const handleClickBackBtn = () => {
+    navigate('/users');
+  };
 
   if (isLoading) return <div>loading</div>;
   if (!user) return <div>undefined user, this id is {id}.</div>;
-  return <User user={user} onClickBackBtn={console.log} />;
+  return <User user={user} onClickBackBtn={handleClickBackBtn} />;
 }
