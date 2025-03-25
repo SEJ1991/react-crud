09996b69d656137af31e575ff1d1e3a42c9a07fb
SEJ <jink6111@gmail.com>
2025-03-25 16:41:04 +0900

    feat: [RC_publish-CardLoading-component-32] complate publish CardLoading component

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -2,5 +2,6 @@ export * from './apis/jsonplaceholder';
 
 export * from './components/Error';
 export * from './components/Loading';
+export * from './components/CardLoading';
 export * from './components/PageFrame';
 export * from './components/AtomicCard';
