e99a1cdd0cfb0e28d5473780b094de1d5d877512
SEJ <jink6111@gmail.com>
2025-03-25 16:23:57 +0900

    refactor: [RC_publish-Loading-component-30] refactor Loading component style

--- src/shared/components/Loading.tsx
+++ src/shared/components/Loading.tsx
@@ -1,9 +1,10 @@
 import { ComponentPropsWithoutRef } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 export function Loading(props: ComponentPropsWithoutRef<'div'>) {
   return (
-    <div {...props} className='animate-bounce text-lg font-semibold'>
-      🐣 Loading 🐣
+    <div {...props} className={twMerge('size-full flex-center absolute inset-0', props.className)}>
+      <span className='animate-bounce text-lg font-semibold'>🐣 Loading 🐣</span>
     </div>
   );
 }
