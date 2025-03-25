8e03855b252fc761682ac1c572cc7481242feb16
SEJ <jink6111@gmail.com>
2025-03-25 17:07:30 +0900

    refactor: [RC_refactor-card-style-35] remove mb-4 in card class

--- src/domains/post/components/Post.tsx
+++ src/domains/post/components/Post.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 export default function Post({ post: { title, body }, onClickBackBtn }: Props) {
   return (
-    <AtomicCard>
+    <AtomicCard className='mb-4'>
       <button
         className='transition-colors duration-500 ease-in-out hover:text-red-400'
         onClick={onClickBackBtn}
